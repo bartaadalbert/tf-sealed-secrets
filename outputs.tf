dd8f331c9563aeee4a7bf22dcec5fa74d4a54ae3
Adalbert Barta <adalbertbarta@gmail.com>
2023-10-15 23:08:50 +0200

    outputs all_encrypted_secrets

--- outputs.tf
+++ outputs.tf
@@ -29,3 +29,14 @@ output "public_key_pem" {
   description = "The public key ready."
   value       = data.local_file.public_key_pem.content
 }
+
+data "local_file" "all_encrypted_secrets" {
+  depends_on = [null_resource.concatenate_encrypted_secrets]
+  filename = "${path.module}/all-encrypted-secrets.yaml"
+}
+
+output "all_encrypted_secrets" {
+  depends_on = [null_resource.concatenate_encrypted_secrets]
+  description = "All encrypted secrets data."
+  value       = data.local_file.all_encrypted_secrets.content
+}
